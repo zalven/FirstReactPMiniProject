9f57e9f0e11e6ecc9ab4599760c80f0200d4bc3e
unknown <zalvendayao@gmail.com>
2020-08-13 21:35:24 +0800

    Updates

---
+++ precache-manifest.05e36b7174b0f0565a0b798c94e153f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3525e7f4ce667a707dbb03c6ac6f8d24",
+    "url": "/FirstReactPMiniProject/index.html"
+  },
+  {
+    "revision": "580b921f85d7da16027f",
+    "url": "/FirstReactPMiniProject/static/css/main.0beab84b.chunk.css"
+  },
+  {
+    "revision": "a4e76360ec6f150cc6bb",
+    "url": "/FirstReactPMiniProject/static/js/2.cb486250.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/FirstReactPMiniProject/static/js/2.cb486250.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "580b921f85d7da16027f",
+    "url": "/FirstReactPMiniProject/static/js/main.51ed66fb.chunk.js"
+  },
+  {
+    "revision": "dbaf0efee14c34defb4a",
+    "url": "/FirstReactPMiniProject/static/js/runtime-main.b7b2e595.js"
+  }
+]);
